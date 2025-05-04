fd49a1fc6c1068aa996eaca9220ceefab47cd127
RedPillCoder1 <tanishamitshah@gmail.com>
2025-05-05 03:00:42 +0530

    fix settings for render deployment

--- Event_Feedback_analyzer/settings.py
+++ Event_Feedback_analyzer/settings.py
@@ -21,11 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-564q$fs6%^jh3@)%9ud2bt3&%u=w1d8l%4$wypc%gxr)m%u_o!"
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = ['event-feedback-analyzer-3ysx.onrender.com']
 
